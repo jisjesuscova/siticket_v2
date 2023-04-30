2233f09ceec42cf09579fcbcb6299351ab489588
jisjesuscova <jesuscova@jisparking.com>
2023-04-29 21:49:52 -0400

    Initial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,8 +29,6 @@
 
   <!-- jQuery -->
   <script src="/template/plugins/jquery/jquery.min.js"></script>
-  <!-- Bootstrap -->
-  <script src="/template/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
   <!-- AdminLTE -->
   <script src="/template/dist/js/adminlte.js"></script>
 
