56444b1924ef5c14f6ad123f4fbc1610cad3a889
jisjesuscova <jesuscova@jisparking.com>
2023-04-30 12:40:18 -0400

    cambios

--- resources/js/project/components/Home.vue
+++ resources/js/project/components/Home.vue
@@ -5,7 +5,7 @@
       <div class="container-fluid">
         <div class="row">
           <div class="col-lg-12 text-center">
-            
+            <img :src="`../../template/dist/img/logo.jpg`" id="home_logo" alt="Descripción de la imagen">
           </div>
         </div>
       </div>
