8fd6d18982f74119c2c6cf6d70f1c0b81f5c714f
jisjesuscova <jesuscova@jisparking.com>
2023-05-07 00:32:02 -0400

    cambios

--- resources/js/project/components/CheckTicket.vue
+++ resources/js/project/components/CheckTicket.vue
@@ -14,16 +14,11 @@
                 <div class="row">
                     <div class="col-12">
                         <div class="card">
-                            <img :src="`../../template/dist/img/logo.jpg`" style="height: auto; width: auto;" alt="Descripción de la imagen">
-                            <br><br>
                             <center>
-                                <h1>Evento: <strong>Mana</strong>.</h1>
-                                <h1>Fecha 12/01/2023</h1>
-                                <br><br>
-                                <div class="alert alert-success text-center" role="alert">
+                                <div v-if="status === 1" class="alert alert-success text-center" role="alert">
                                     <h3><i class="fa-regular fa-circle-check"></i> Entrada aceptada</h3>
                                 </div>
-                                <div class="alert alert-danger text-center" role="alert">
+                                <div v-if="status === 0" class="alert alert-danger text-center" role="alert">
                                     <h3><i class="fa-solid fa-circle-exclamation"></i> Entrada denegada</h3>
                                 </div>
                             </center>
@@ -38,13 +33,14 @@
 <script>
 export default {
     data() {
-
+        return {
+            status: null
+        }
     },
     methods: {
         checkTicket() {
             this.$axios.get('/api/ticket/check/'+ this.$route.params.id).then((res) => {
                 this.status = res.data.data;
-                console.log(this_status);
             })
         }
     },
